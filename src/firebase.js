f6e4a6337b2706da9f01b8e5f84d9d8705dd609b
emnaghzaiel176@gmail.com <emnaghzaiel176@gmail.com>
2024-01-24 17:23:41 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJhRJ-FRVojZD5r1GtmvuSJF-SgxK6JC4",
+  authDomain: "netflix-clone-8eb41.firebaseapp.com",
+  projectId: "netflix-clone-8eb41",
+  storageBucket: "netflix-clone-8eb41.appspot.com",
+  messagingSenderId: "942129639300",
+  appId: "1:942129639300:web:d83e821a9692d6dbfd37d1",
+};
