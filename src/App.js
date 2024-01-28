f22a3ef77c3dbae4dde2590775f9cacf0800b83b
emnaghzaiel176@gmail.com <emnaghzaiel176@gmail.com>
2024-01-29 00:39:20 +0100

    add react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomeScreen from "./pages/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        <Routes>
+        <Route path="/" element={ <HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
